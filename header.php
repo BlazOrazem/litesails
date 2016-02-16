ee074554d464ef2c17156a7ab8d0fde7ad50822c
Blaž Oražem <blaz@orazem.info>
2016-02-16 20:24:49 +0100

    Fixed meta data.

--- header.php
+++ header.php
@@ -2,16 +2,10 @@
 <!DOCTYPE html>
 <html>
 <head lang="en">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
     <meta charset="UTF-8">
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no"/>
-    <meta name="description" content="Calculate your travel expenses with Fuel Consumption Calculator."/>
-    <meta name="keywords" content="fuel, consumption, calculator">
+    <meta name="description" content="Wind, weather and sea forecast for Adriatic coast."/>
+    <meta name="keywords" content="weather forecast, wind forecast, sea forecast, adriatic sea">
     <meta name="author" content="Blaž Oražem">
 
     <title>Lite Sails</title>
