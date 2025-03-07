8b174381aa07bd90674845301b75771ab1f242fc
Blaz Orazem <blaz@orazem.si>
2025-03-07 16:05:11 +0100

    Add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'blazorazem/litesails',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '23aaeff9cf39ca0e693dd38cb18f23a0b922fdf3',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'blazorazem/litesails' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '23aaeff9cf39ca0e693dd38cb18f23a0b922fdf3',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'electrolinux/phpquery' => array(
+            'pretty_version' => '0.9.7',
+            'version' => '0.9.7.0',
+            'reference' => '90029e2acffebebdb6e309509c778fe34e8028ba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../electrolinux/phpquery',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
